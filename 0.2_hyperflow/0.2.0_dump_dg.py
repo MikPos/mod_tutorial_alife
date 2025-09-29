73f5ad7140e57e181255ced4533bdac5803bdafb
Anne-Susann Abel <118827492+anne-susann@users.noreply.github.com>
2025-09-29 11:47:12 +0200

    Updated flow with running version
    With examples

---
+++ 0.2_hyperflow/0.2.0_dump_dg.py
@@ -0,0 +1,26 @@
+### create a dg and dump the files 
+
+include("grammar_formose.py")
+
+
+strat = (
+	addUniverse(formaldehyde)
+	>> addSubset(glycolaldehyde)
+	# Constrain the reactions:
+	# No molecules with more than 20 atoms can be created.
+	>> rightPredicate[
+		lambda derivation: all(g.numVertices <= 20 for g in derivation.right)
+	](
+		# Iterate until nothing new is found.
+		repeat(
+			inputRules
+		)
+	)
+)
+
+dg = DG(graphDatabase=inputGraphs)
+dg.build().execute(strat)
+dg.print()
+
+
+dg.dump("dg_autocat.dg")
