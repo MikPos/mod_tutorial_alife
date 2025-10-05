56ef51d5e879d7e663e499a1eca3ae37e8a1214a
Anne-Susann Abel <118827492+anne-susann@users.noreply.github.com>
2025-10-05 09:26:24 +0200

    Update formose.py

--- 0.2_hyperflow/formose.py
+++ 0.2_hyperflow/formose.py
@@ -5,15 +5,12 @@ glycolaldehyde = Graph.fromSMILES( "OCC=O", name="Glycolaldehyde")
 
 inputGraphs = [formaldehyde, glycolaldehyde]
 
-# define molecules for flow queries
-example_mol = Graph.fromSMILES("C(CO)(CO)=O", name="example molecule")
-
-
 # import rules from GML files:
 ketoEnol_F = Rule.fromGMLFile("rules/keto_enol_univ.gml")
 ketoEnol_B = Rule.fromGMLFile("rules/keto_enol_univ.gml", invert=True)
 aldolAdd_F = Rule.fromGMLFile("rules/aldol_add_univ.gml")
 aldolAdd_B = Rule.fromGMLFile("rules/aldol_add_univ.gml", invert=True)
+
 
 
 
